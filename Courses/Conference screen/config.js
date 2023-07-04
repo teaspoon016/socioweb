1a3bd60fc8d904c55c3d96f79dbba63fe0e1e49f
Terrence Eze <84102411+teaspoon016@users.noreply.github.com>
2023-07-04 12:23:37 +0100

    added call

---
+++ Courses/Conference screen/config.js
@@ -0,0 +1,2 @@
+// Auth token we will use to generate a meeting and connect to it
+TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI3MGUzYWI1Zi1mOWI1LTQ4NDMtODhkMS1kYzhmZTI3N2E2ZDkiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTY4Nzc5MjYwOCwiZXhwIjoxODQ1NTgwNjA4fQ.szscvJ6EoXroARfOnnoaUDvckTiGsg4OpCmKyG796SI";
